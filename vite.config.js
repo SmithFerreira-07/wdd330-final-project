f03dd80ca61f9ab92d80605bea2c4bf520e607a4
SmithFerreira-07 <GitFerreira@proton.me>
2025-10-04 19:29:26 -0300

    fixed paths and config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import { fileURLToPath } from "url";
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
+  root: "./",
   build: {
+    outDir: "dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
